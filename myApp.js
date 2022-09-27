042b90699a9b017f559f1cfedcf323dbc7e8538e
sumeet <30768189+sumeetuke@users.noreply.github.com>
2022-09-27 11:56:36 +0530

    changes
    don changes made

--- myApp.js
+++ myApp.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+  console.log("successfully connected to DATABASE")});
 
 
 let Person;
